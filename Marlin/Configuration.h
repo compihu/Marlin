38cdcab9621a765a93d7b945fa9ae5130e21a426
Attila BODY <attila.body@gmail.com>
2017-08-26 10:58:17 +0200

    PID tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -335,10 +335,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
+  // FLSun Cube
+  #define  DEFAULT_Kp 24.64
+  #define  DEFAULT_Ki 1.86
+  #define  DEFAULT_Kd 81.74
+  
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+  //#define  DEFAULT_Kp 22.2
+  //#define  DEFAULT_Ki 1.08
+  //#define  DEFAULT_Kd 114
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
@@ -380,9 +385,12 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
+  #define  DEFAULT_bedKp 641.42
+  #define  DEFAULT_bedKi 111.96
+  #define  DEFAULT_bedKd 918.67
+  //#define  DEFAULT_bedKp 189.46
+  //#define  DEFAULT_bedKi 33.52
+  //#define  DEFAULT_bedKd 267.74
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
@@ -1251,7 +1259,7 @@
 //
 //  Set this option if CLOCKWISE causes values to DECREASE
 //
-//#define REVERSE_ENCODER_DIRECTION
+#define REVERSE_ENCODER_DIRECTION
 
 //
 // This option reverses the encoder direction for navigating LCD menus.
