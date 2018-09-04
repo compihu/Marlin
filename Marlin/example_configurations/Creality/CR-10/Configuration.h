930608176155a13666740fec7e74aba0a59144fa
Attila BODY <attila.body@gmail.com>
2018-09-04 23:29:30 +0200

    Z leadscrew 8mm -> 2mm, height to 380mm

--- Marlin/example_configurations/Creality/CR-10/Configuration.h
+++ Marlin/example_configurations/Creality/CR-10/Configuration.h
@@ -608,14 +608,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 137 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 137 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 7, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -647,7 +647,7 @@
  */
 #define DEFAULT_XJERK                 10.0
 #define DEFAULT_YJERK                 10.0
-#define DEFAULT_ZJERK                  2.7
+#define DEFAULT_ZJERK                  2.0
 #define DEFAULT_EJERK                  5.0
 
 /**
@@ -888,7 +888,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 300
+#define Z_MAX_POS 380
 
 /**
  * Software Endstops
